6e58fbbcf79a25b467369fe476ae6df63c947cb1
Only-Vitin <vitinhosantossilva123@gmail.com>
2024-02-29 18:08:53 -0300

    data annotations

--- EmailServiceApi/Dto/EmailDto.cs
+++ EmailServiceApi/Dto/EmailDto.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EmailServiceApi.Dto
 {
     public class EmailDto
     {
+        [Required]
         public string From { get; set; }
+
+        [Required]
         public string To { get; set; }
+
+        [Required]
         public string Subject { get; set; }
+
+        [Required]
         public string Body { get; set; }
     }
 }
--- RegistrationApi/Dto/UserDto.cs
+++ RegistrationApi/Dto/UserDto.cs
@@ -1,17 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RegistrationApi.Dto
 {
     public class UserDto
     {
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Informe o nome", AllowEmptyStrings = false)]
         public string Name { get;  set; }
+
+        [Display(Name = "Data de nascimento")]
+        [Required(ErrorMessage = "Informe a data de nascimento", AllowEmptyStrings = false)]
+        [DataType(DataType.DateTime)]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Gênero")]
+        [Required(AllowEmptyStrings = true)]
         public string Gender { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "Informe o CPF", AllowEmptyStrings = false)]
+        [RegularExpression(@"^([0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2})|(^[0-9]{11})$")]
         public string CPF { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Informe o email", AllowEmptyStrings = false)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^((([a-z0-9_\-\.]+)@(([a-z0-9_\-]+)\.)+([a-z]{2,6})))|^((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\]))$")]
         public string Email { get;  set; }
+
+        [Display(Name = "Senha")]
+        [Required(ErrorMessage = "Informe o nome do usuário", AllowEmptyStrings = false)]
+        [DataType(DataType.Password)]
         public string Password { get;  set; }
-        public int Type { get; set; } 
+
+        [Display(Name = "1 - Cliente | 2 - Funcionário")]
+        [Required(ErrorMessage = "Informe o tipo do usuário", AllowEmptyStrings = false)]
+        [Range(1, 2)]
+        public int Type { get; set; }
+
+        [Display(Name = "Campos específicos")]
+        [Required(AllowEmptyStrings = false)]
         public Dictionary<string, string> Fields { get; set; }
     }
 }
